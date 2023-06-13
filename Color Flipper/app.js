1ffc92974dd6bfce1231f340700a91495e926769
Simran Ahuja <97106836+SimranAhuja00@users.noreply.github.com>
2023-06-13 21:13:48 +0530

    Add files via upload

---
+++ Color Flipper/app.js
@@ -0,0 +1,13 @@
+const colors = ['green','red', 'white','aqua','blue','azure','black','bisque','beige','blueviolet','brown','crimson'];
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click',function(){
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random()* colors.length);
+}
