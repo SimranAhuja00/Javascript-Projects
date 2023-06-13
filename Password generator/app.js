a836b17994a506b33853eb026fa72c9d5d9a840f
Simran Ahuja <97106836+SimranAhuja00@users.noreply.github.com>
2023-06-14 00:47:08 +0530

    Add files via upload

---
+++ Password generator/app.js
@@ -0,0 +1,53 @@
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "123456789";
+const symbol = "~!@#$%^&*()_+/";
+
+//selectors
+const passBox = document.getElementById("pass-box")
+const totalChar = document.getElementById("total-char")
+const upperInput = document.getElementById('upper-case')
+const lowerInput = document.getElementById('lower-case')
+const numberInput = document.getElementById("numbers")
+const symbolInput = document.getElementById("symbols")
+
+
+const getRandomData = (dataSet) => {
+    return dataSet[Math.floor(Math.random() * dataSet.length)]
+}
+
+const generatePassword = (password = "") => {
+    if(upperInput.checked){
+        password += getRandomData(upperCase);
+    }
+    if (lowerInput.checked) {
+        password += getRandomData(lowerCase)
+    }
+    if (numberInput.checked) {
+        password += getRandomData(numbers)
+    }
+    if (symbolInput.checked) {
+        password += getRandomData(symbol)
+    }
+    if (password.length < totalChar.value) {
+        return generatePassword(password)
+    }
+    passBox.innerText = truncateString(password, totalChar.value);
+
+}
+
+document.getElementById('btn').addEventListener(
+    "click",
+    function () {
+        generatePassword();
+    }
+)
+
+function truncateString(str, num) {
+    if (str.length > num) {
+        let subStr = str.substring(0, num);
+        return subStr;
+    } else {
+        return str;
+    }
+}
